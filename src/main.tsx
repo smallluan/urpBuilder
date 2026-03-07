40630491f68808a8ae5ab39b48830e8bfc053b9c
ts <598051800@qq.com>
2026-03-07 14:21:34 +0800

    fix(rest): 引入 reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './styles/reset.less'
 import 'tdesign-react/dist/tdesign.min.css'
 import App from './App.tsx'
 
