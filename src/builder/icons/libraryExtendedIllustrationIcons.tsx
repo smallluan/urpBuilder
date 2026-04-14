a83a3c7c60c8b83cd5b98f862f66ac48642061d0
ts <598051800@qq.com>
2026-04-14 12:07:02 +0800

    feat: 优化图标组件和预览引擎路由
    - 完善 libraryActionBasicIcons.tsx 基础操作图标
    - 更新 libraryExtendedIllustrationIcons.tsx 扩展插画图标
    - 优化预览引擎页面路由配置
    - 改进整体路由结构和导航体验

--- src/builder/icons/libraryExtendedIllustrationIcons.tsx
+++ src/builder/icons/libraryExtendedIllustrationIcons.tsx
@@ -1,4 +1,4 @@
-import React, { useId } from 'react';
+import { useId } from 'react';
 import { LibSvg, type LibraryGlyphProps } from './libraryIllustrationPrimitives';
 import { LibIllustrationTheme as T } from './libraryIllustrationTheme';
 
